5792013308d0abc6de6d7fc06695973b1a7ea2fa
Sajid Anwar <sajidanwar94@gmail.com>
2016-10-23 15:56:00 -0400

    Add D3 TypeScript definitions, example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-let test: string = 'Hello World';
+/// <reference path='d3.d.ts' />
 
-console.log(test);
+// Based on bar chart tutorial
+// https://bost.ocks.org/mike/bar/
+
+// Configure our visualization
+let width = 420;
+let barHeight = 20;
+let data = [4, 8, 15, 16, 23, 42];
+
+let chart = d3.select('.chart')
+    .attr('width', width)
+    .attr('height', data.length * barHeight);
+
+// Create the x-axis's scale
+let x = d3.scaleLinear()
+    .range([0, width])
+    .domain([0, d3.max(data)]);
+
+// Create our data join
+let bar = chart.selectAll('g').data(data);
+
+let barEnter = bar.enter()
+  .append('g')
+    .attr('transform', (d, i) => 'translate(0,' + i * barHeight + ')');
+
+barEnter.append('rect')
+    .attr('width', d => x(d))
+    .attr('height', barHeight - 1);
+    
+barEnter.append('text')
+    .attr('x', d => x(d) - 3)
+    .attr('y', barHeight / 2)
+    .attr('dy', '.35em')
+    .text(d => '' + d);
