579e6df477db784558b5abded585daf8c4f9a53d
Sajid Anwar <sajidanwar94@gmail.com>
2016-11-02 13:16:14 -0400

    Add listings CSV, loading

--- src/application.ts
+++ src/application.ts
@@ -2,7 +2,7 @@ import * as d3 from './d3';
 
 import { Dispatch, DispatchEvent, LoadEventData } from './data/dispatch';
 import { NeighborhoodGeoJSON } from './data/geojson';
-import { Listing, Neighborhood } from './data/listing';
+import { Listing, Neighborhood, ListingCSVRow } from './data/listing';
 
 import * as components from './components/';
 
@@ -27,19 +27,43 @@ export class Application {
 
     private loadData() {
         let neighborhoods = new Map<Neighborhood.NameType, Neighborhood>();
-        neighborhoods.set('South of Market', { name: 'South of Market', listings: []});
-
         let listings = new Map<Listing.IDType, Listing>();
-        listings.set(0, null);
 
-        d3.json('data/neighborhoods.geojson', (error, data: NeighborhoodGeoJSON) => {
-            let loadData: LoadEventData = {
-                geo: data,
-                neighborhoods: neighborhoods,
-                listings: listings
-            };
+        // Load the neighborhood JSON and listings JSON
+        d3.json('data/neighborhoods.geojson', (error, geo: NeighborhoodGeoJSON) => {
+            d3.csv(
+                'data/listings.csv', 
+                (row, index, columns) => row as ListingCSVRow,
+                (error, data) => {
+                    // Process the listing data
+                    for (let row of data) {
+                        // Get the neighborhood for this listing
+                        let neighborhood = neighborhoods.get(row.neighbourhood_cleansed);
 
-            this.dispatcher.call(DispatchEvent.Load, undefined, loadData)
+                        // If the neighborhood does not yet exist, create it
+                        if (neighborhood === undefined) {
+                            neighborhood = {
+                                name: row.neighbourhood_cleansed,
+                                listings: []
+                            };
+                            neighborhoods.set(neighborhood.name, neighborhood);
+                        }
+
+                        // Create our current listing and add it to the array and the right neighborhood
+                        let listing = Listing.parseCSVRow(row, neighborhood);
+                        neighborhood.listings.push(listing);
+                        listings.set(listing.id, listing);
+                    }
+
+                    let loadData: LoadEventData = {
+                        geo: geo,
+                        neighborhoods: neighborhoods,
+                        listings: listings
+                    };
+
+                    this.dispatcher.call(DispatchEvent.Load, undefined, loadData)
+                }
+            );
         });
     }
 }
--- src/data/listing.ts
+++ src/data/listing.ts
@@ -1,5 +1,42 @@
+import * as d3 from '../d3';
+
 export module Listing {
     export type IDType = number;
+    export function parseCSVRow(row: d3.DSVRowString, neighborhood: Neighborhood) : Listing {
+        return {
+            id: +row['id'],
+            name: row['name'],
+            description: row['description'],
+            neighborhood: neighborhood,
+            amenities: row['amenities'],
+            cancellation_policy: row['cancellation_policy'],
+            reviews: {
+                number: +(row['number_of_reviews']),
+                numberPerMonth: +(row['reviews_per_month']),
+                rating: parseInt(row['review_scores_rating']),
+                scores: {
+                    accuracy: +row['review_scores_accuracy'],
+                    cleanliness: +row['review_scores_cleanliness'],
+                    checkin: +row['review_scores_checkin'],
+                    communication: +row['review_scores_communication'],
+                    location: +row['review_scores_location'],
+                    value: +row['review_scores_value']
+                }
+            },
+            prices: {
+                markup_amount: +row['rent_difference'],
+                markup_percentage: +row['rent_difference_percentage_of_median'] * 100,
+                airbnb: {
+                    daily: +row['price'],
+                    monthly: +row['airbnb_monthly_rent'],
+                    monthly_per_bedroom: +row['airbnb_monthly_rent_per_bedroom']
+                },
+                trulia: {
+                    rent_per_bedroom: +row['trulia_median_rent_per_bedroom']
+                }
+            }
+        };
+    };
 }
 
 export module Neighborhood {
@@ -8,7 +45,36 @@ export module Neighborhood {
 
 export interface Listing {
     id: Listing.IDType;
-    neighborhoods: Neighborhood[];
+    name: string;
+    description: string;
+    neighborhood: Neighborhood;
+    amenities: string;
+    cancellation_policy: string;
+    reviews: {
+        number: number;
+        numberPerMonth: number;
+        rating: number;
+        scores: {
+            accuracy: number;
+            cleanliness: number;
+            checkin: number;
+            communication: number;
+            location: number;
+            value: number;
+        }
+    };
+    prices: {
+        markup_amount: number;
+        markup_percentage: number;
+        airbnb: {
+            daily: number;
+            monthly: number;
+            monthly_per_bedroom: number;
+        };
+        trulia: {
+            rent_per_bedroom: number;
+        };
+    }
 }
 
 export interface Neighborhood {
