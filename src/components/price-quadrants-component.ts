5824a6db5136fc21bbd34b7ca892d8d43bc21dd3
Sajid Anwar <sajidanwar94@gmail.com>
2016-11-08 00:23:48 -0500

    Quadrants: show neighborhood scatter plots

--- src/components/price-quadrants-component.ts
+++ src/components/price-quadrants-component.ts
@@ -6,9 +6,14 @@ import { Listing, Neighborhood } from '../data/listing';
 
 export class PriceQuadrantsComponent extends BaseComponent {
     
+    private listings: Listing[];
+    private neighborhoods: Neighborhood[];
+
     private view: {
         svg?: d3.Selection<d3.BaseType, {}, d3.BaseType, {}>;
         padding?: d3.Padding;
+        
+        neighborhoodCircles?: d3.Selection<d3.BaseType, Neighborhood, d3.BaseType, {}>;
     }
 
     public constructor(selector: string, dispatcher: Dispatch) {
@@ -28,6 +33,10 @@ export class PriceQuadrantsComponent extends BaseComponent {
 
     public onLoad(data: LoadEventData) {
         super.onLoad(data);
+
+        this.listings = Array.from(this.data.listings.values());
+        this.neighborhoods = Array.from(this.data.neighborhoods.values());
+
         this.render();
     }
 
@@ -37,6 +46,13 @@ export class PriceQuadrantsComponent extends BaseComponent {
 
     public onHighlight(highlight: HighlightEventData) {
         super.onHighlight(highlight);
+
+        this.view.neighborhoodCircles.attr('fill', d => {
+            if (highlight.neighborhood === d) 
+                return 'rgba(255, 100, 100, 0.5)';
+            else
+                return 'rgba(50, 50, 100, 0.5)';
+        });
     }
 
     public onFilter(filter: FilterEventData) {
@@ -57,12 +73,16 @@ export class PriceQuadrantsComponent extends BaseComponent {
         let innerPadding = d3.Padding.add(this.view.padding, new d3.Padding(0, 40, 40, 0));
 
         let markupScale = d3.scaleLinear()
-            .domain([0, 100])
-            .range([innerPadding.height(height), 0]);
+            .domain(d3.extent(this.neighborhoods, n => d3.mean(n.listings, l => l.prices.markup_percentage)))
+            .range([innerPadding.height(height) + innerPadding.top, innerPadding.top]);
 
         let otherScale = d3.scaleLinear()
-            .domain([0, 100])
-            .range([0, innerPadding.width(width)]);
+            .domain(d3.extent(this.neighborhoods, n => d3.mean(n.listings, l => l.reviews.rating)))
+            .range([innerPadding.left, innerPadding.left + innerPadding.width(width)]);
+
+        let countScale = d3.scaleLinear()
+            .domain(d3.extent(this.neighborhoods, n => n.listings.length))
+            .range([5, 30]);
 
         let markupAxis = d3.axisLeft(markupScale);
         let otherAxis = d3.axisBottom(otherScale);
@@ -71,13 +91,13 @@ export class PriceQuadrantsComponent extends BaseComponent {
         this.view.svg
           .append('g')
             .attr('class', 'markup-axis')
-            .style('transform', innerPadding.translate(0, 0))
+            .style('transform', innerPadding.translateX(0))
             .call(markupAxis);
 
         this.view.svg
           .append('g')
             .attr('class', 'other-axis')
-            .style('transform', innerPadding.translate(0, innerPadding.height(height)))
+            .style('transform', innerPadding.translateY(innerPadding.height(height)))
             .call(otherAxis);
 
         // Draw axis labels
@@ -95,5 +115,38 @@ export class PriceQuadrantsComponent extends BaseComponent {
             .style('transform', `translate(${innerPadding.centerX(width)}px, ${height - this.view.padding.bottom}px)`)
           .append('text')
             .text('Rating');
+
+        // Draw the neighborhoods
+        this.view.neighborhoodCircles = this.view.svg
+          .selectAll('circle')
+            .data(this.neighborhoods)
+          .enter()
+          .append('circle')
+            .attr('cx', d => otherScale(d3.mean(d.listings, l => l.reviews.rating)))
+            .attr('cy', d => markupScale(d3.mean(d.listings, l => l.prices.markup_percentage)))
+            .attr('r', d => countScale(d.listings.length))
+            .attr('fill', 'rgba(50, 50, 100, 0.5)')
+            .on('mouseenter', function(d) {
+                // Dispatch a highlight event for this neighborhood
+                self.dispatcher.call(DispatchEvent.Highlight, this, {
+                    neighborhood: d,
+                    listing: undefined
+                } as HighlightEventData);
+
+                // Scale up the particular neighborhood. 
+                let sel = d3.select(this);
+                sel.moveToFront();
+                sel.style('fill', 'rgba(255, 100, 100, 0.5)');
+            })
+            .on('mouseleave', function(d) {
+                // Dispatch an empty highlight event
+                self.dispatcher.call(DispatchEvent.Highlight, this, {
+                    neighborhood: undefined,
+                    listing: undefined
+                } as HighlightEventData);
+
+                let sel = d3.select(this);
+                sel.style('fill', 'rgba(50, 50, 100, 0.5)');
+            });
     }
 } 
