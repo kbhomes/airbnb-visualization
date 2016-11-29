938c57b090ae4729d341772e805c5a44f488ade7
Sajid Anwar <sajidanwar94@gmail.com>
2016-11-29 17:22:54 -0500

    Show all types of selections in the filter list

--- src/components/filters-component.ts
+++ src/components/filters-component.ts
@@ -4,18 +4,32 @@ import { BaseComponent } from './base-component';
 import { Dispatch, DispatchEvent, LoadEventData, SelectEventData, HighlightEventData, FilterEventData } from '../data/dispatch';
 import { Attribute } from '../data/attribute';
 import { Listing, Neighborhood } from '../data/listing';
+import { Block } from '../data/block';
 
 export class FiltersComponent extends BaseComponent {
 
     private view: {
-        neighborhoodFiltersList?: d3.Selection<d3.BaseType, Neighborhood, d3.BaseType, {}>;
+        neighborhoodFiltersList?: d3.DataSelection<Neighborhood>;
+        listingsFilterList?: d3.DataSelection<Listing>;
+        priceBlocksFilterList?: d3.DataSelection<Block>;
+        markupBlocksFilterList?: d3.DataSelection<Block>;
+        description?: d3.DatalessSelection;
     }
 
     public constructor(selector: string, dispatcher: Dispatch) {
         super(selector, dispatcher);
 
         this.view = {};
-    }
+        this.view.description = d3.select(this.element).select('.filter-description');
+        this.view.description.select('.reset').on('click', () => {
+            this.dispatcher.call(DispatchEvent.Select, this, {
+                neighborhoods: undefined,
+                listings: undefined,
+                priceBlocks: undefined,
+                markupBlocks: undefined
+            } as SelectEventData);
+        });
+    }  
 
     public onSelect(selection: SelectEventData) {
         super.onSelect(selection);
@@ -26,7 +40,7 @@ export class FiltersComponent extends BaseComponent {
 
     }
 
-    public render() {
+    private renderNeighborhoods() {
         let self = this;
 
         let filterSelection = d3.select(this.selector)
@@ -45,5 +59,141 @@ export class FiltersComponent extends BaseComponent {
 
         let filterExit = filterSelection.exit().remove();
         this.view.neighborhoodFiltersList = filterSelection.merge(filterEnter);
+    }
+
+    private renderListings() {
+        let self = this;
+        
+        let filterSelection = d3.select(this.selector)
+            .select('.filter-listings')
+            .selectAll('div')
+            .data(this.selection.listings || [], (d: Listing) => d.id + '');
+
+        let filterEnter = filterSelection
+          .enter()
+          .append('div')
+            .text(d => d.name)
+            .on('click', function(d) {
+                // Send out a deselection event for this neighborhood
+                self.dispatchListingSelection(d);
+            });
+
+        let filterExit = filterSelection.exit().remove();
+        this.view.listingsFilterList = filterSelection.merge(filterEnter);
+    }
+
+    private renderPriceBlocks() {
+        let self = this;
+        
+        let filterSelection = d3.select(this.selector)
+            .select('.filter-price-blocks')
+            .selectAll('div')
+            .data(this.selection.priceBlocks || [], (d: Block) => d.number + '');
+
+        let filterEnter = filterSelection
+          .enter()
+          .append('div')
+            .text(d => {
+                let label = '$' + d.minimum.toFixed(0);
+
+                if (isNaN(d.maximum)) {
+                    label += '+';
+                } 
+                else {
+                    label += ' - $' + d.maximum.toFixed(0);
+                }
+
+                return label;
+            })
+            .on('click', function(d) {
+                // Send out a deselection event for this neighborhood
+                self.dispatchBlockSelection(d);
+            });
+
+        let filterExit = filterSelection.exit().remove();
+        this.view.priceBlocksFilterList = filterSelection.merge(filterEnter);
+    }
+
+    private renderMarkupBlocks() {
+        let self = this;
+        
+        let filterSelection = d3.select(this.selector)
+            .select('.filter-markup-blocks')
+            .selectAll('div')
+            .data(this.selection.markupBlocks || [], (d: Block) => d.number + '');
+
+        let filterEnter = filterSelection
+          .enter()
+          .append('div')
+            .text(d => {
+                let label = d.minimum.toFixed(0);
+
+                if (isNaN(d.maximum)) {
+                    label += '+%';
+                } 
+                else {
+                    label += '% - ' + d.maximum.toFixed(0) + '%';
+                }
+
+                return label;
+            })
+            .on('click', function(d) {
+                // Send out a deselection event for this neighborhood
+                self.dispatchBlockSelection(d);
+            });
+
+        let filterExit = filterSelection.exit().remove();
+        this.view.markupBlocksFilterList = filterSelection.merge(filterEnter);
+    }
+
+    private renderFilterDescription() {
+        let pluralize = (count: number, word: string) => {
+            let label = count + ' ';
+            
+            if (count === 1)
+                label += word;
+            else 
+                label += word + 's';
+
+            return label;
+        };
+
+        if (this.selection.neighborhoods && this.selection.neighborhoods.length) {
+            this.view.description
+                .style('display', 'block')
+              .select('.count')
+                .text(pluralize(this.selection.neighborhoods.length, 'neighborhood'));
+        }
+        else if (this.selection.listings && this.selection.listings.length) {
+            this.view.description
+                .style('display', 'block')
+              .select('.count')
+                .text(pluralize(this.selection.listings.length, 'listing'));
+        }
+        else if (this.selection.priceBlocks && this.selection.priceBlocks.length) {
+            this.view.description
+                .style('display', 'block')
+              .select('.count')
+                .text(pluralize(this.selection.priceBlocks.length, 'price block'));
+        }
+        else if (this.selection.markupBlocks && this.selection.markupBlocks.length) {
+            this.view.description
+                .style('display', 'block')
+              .select('.count')
+                .text(pluralize(this.selection.markupBlocks.length, 'markup block'));
+        }
+        else {
+            this.view.description.style('display', 'none');
+        }
+    }
+
+    public render() {
+        let self = this;
+
+        this.renderNeighborhoods();
+        this.renderListings();
+        this.renderPriceBlocks();
+        this.renderMarkupBlocks();
+        this.renderFilterDescription();
     }
 } 
